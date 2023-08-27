33cec00f529ac4f589f19826542a29e27da6ca35
Kai Yong <okyspace@gmail.com>
2023-08-27 09:04:17 +0800

    Updated

--- datacatalog-app/main.py
+++ datacatalog-app/main.py
@@ -1,23 +1,39 @@
+import argparse
 from time import sleep
-
 from clearml import Task, Dataset
-from clearml.backend_api.session.client import APIClient
 
-task = Task.init(
-    project_name="DataOps",
-    task_name="Datacatalog App",
-    task_type=Task.TaskTypes.service,
-    reuse_last_task_id=False,
-)
+def add_arguments(parser):
+    parser.add_argument(
+        "--search-entry",
+        type=str,
+        default="datasets-mnist",
+        help="search entry",
+    )
 
-logger = task.get_logger()
+def main():
+    parser = argparse.ArgumentParser(description="ClearML Datacatalog: Gather datasets available in ClearML")
+    add_arguments(parser)
+    args = parser.parse_args()
 
-while True:
-    print("Fetching list of Datasets ...")
+    task = Task.init(
+        project_name="DataOps",
+        task_name="Datacatalog App",
+        task_type=Task.TaskTypes.service,
+        reuse_last_task_id=False,
+    )
 
-    datasets = Dataset.get(
-        dataset_name="datasets-mnist", 
-        only_completed=True)
-    task.set_parameter(name='General/datasets_names', value=datasets)
+    logger = task.get_logger()
+
+    while True:
+        print("Fetching list of Datasets ...")
+        datasets = Dataset.get(
+            dataset_name=args.search_entry, 
+            only_completed=True)
+        print("datasets {}".found(datasets))
+        task.set_parameter(name='General/datasets_names', value=datasets)
 
     sleep(10)
+
+
+if __name__ == "__main__":
+    main()
